99e9e291f96da3c1c8dae4f3d28fd9495ff4cacd
Kieran HIll <kieranhill@hotmail.co.uk>
2025-03-29 18:20:34 +0000

    Updated some background stuff Gson

--- app/src/main/java/com/dilligaf/barcodework128/MainActivity.java
+++ app/src/main/java/com/dilligaf/barcodework128/MainActivity.java
@@ -7,7 +7,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
-import com.actualmanx.barcodework128.R;
 
 import androidx.appcompat.app.AppCompatActivity;
 
